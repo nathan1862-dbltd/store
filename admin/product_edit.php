c3eeb37ffd499bdba3e1bd0a534821a79bdba59a
nathan1862-dbltd <kaungkaung1862@icloud.com>
2026-04-26 20:45:33 +0630

    Update product_edit.php

--- admin/product_edit.php
+++ admin/product_edit.php
@@ -1,6 +1,6 @@
 <?php
-require_once __DIR__ . '/../config.php';
-require_once __DIR__ . '/../auth.php';
+require_once __DIR__ . '/adminconfig.php';
+require_once __DIR__ . '/auth.php';
 
 if (!isset($_SESSION)) session_start();
 
