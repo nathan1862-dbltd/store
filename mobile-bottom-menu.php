21a6787d210d71c7a5f79bbdb8d5f527967cc7f4
nathan1862-dbltd <kaungkaung1862@icloud.com>
2026-05-03 10:02:57 +0630

    Update mobile-bottom-menu.php

--- mobile-bottom-menu.php
+++ mobile-bottom-menu.php
@@ -31,16 +31,7 @@ $current_page = basename($_SERVER['PHP_SELF']);
         <a href="shop.php"
            class="nav-item <?php echo ($current_page == 'shop.php') ? 'active' : ''; ?>">
 
-            <svg xmlns="http://www.w3.org/2000/svg"
-                 fill="none"
-                 viewBox="0 0 24 24"
-                 stroke="currentColor">
-
-                <path stroke-linecap="round"
-                      stroke-linejoin="round"
-                      stroke-width="2"
-                      d="M3 3h2l.4 2M7 13h10l4-8H5.4"/>
-            </svg>
+            <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="icon icon-tabler icons-tabler-outline icon-tabler-category"><path stroke="none" d="M0 0h24v24H0z" fill="none" /><path d="M4 4h6v6h-6l0 -6" /><path d="M14 4h6v6h-6l0 -6" /><path d="M4 14h6v6h-6l0 -6" /><path d="M14 17a3 3 0 1 0 6 0a3 3 0 1 0 -6 0" /></svg>
 
         </a>
 
