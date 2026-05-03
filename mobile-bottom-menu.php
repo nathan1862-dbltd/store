5ee5fde75db70364015f9cadbecd4431e2654b40
nathan1862-dbltd <kaungkaung1862@icloud.com>
2026-05-03 10:06:09 +0630

    Update mobile-bottom-menu.php

--- mobile-bottom-menu.php
+++ mobile-bottom-menu.php
@@ -39,16 +39,7 @@ $current_page = basename($_SERVER['PHP_SELF']);
         <a href="add.php"
            class="nav-item <?php echo ($current_page == 'add.php') ? 'active' : ''; ?>">
 
-            <svg xmlns="http://www.w3.org/2000/svg"
-                 fill="none"
-                 viewBox="0 0 24 24"
-                 stroke="currentColor">
-
-                <path stroke-linecap="round"
-                      stroke-linejoin="round"
-                      stroke-width="2"
-                      d="M12 4v16m8-8H4"/>
-            </svg>
+            <svg xmlns:x="http://ns.adobe.com/Extensibility/1.0/" xmlns:i="http://ns.adobe.com/AdobeIllustrator/10.0/" xmlns:graph="http://ns.adobe.com/Graphs/1.0/" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" version="1.1" x="0px" y="0px" viewBox="0 0 100 125" style="enable-background:new 0 0 100 100;" xml:space="preserve"><switch><foreignObject requiredExtensions="http://ns.adobe.com/AdobeIllustrator/10.0/" x="0" y="0" width="1" height="1"/><g i:extraneous="self"><path d="M40.8,8.9L35.7,14h-8.9C19.7,14,14,19.7,14,26.8v8.9l-5.1,5.1c-1.9,1.9-3.3,4.4-3.8,7.1C4.5,52.1,5.8,56.1,8.7,59l5.3,5.3    v8.9C14,80.3,19.7,86,26.8,86h8.9l5.3,5.3c5,5,13,5,18,0l5.3-5.3h8.9C80.3,86,86,80.3,86,73.2v-8.9l5.1-5.1    c1.9-1.9,3.3-4.4,3.8-7.1c0.7-4.1-0.7-8.2-3.6-11.1L86,35.7v-8.9C86,19.7,80.3,14,73.2,14h-8.9L59,8.7c-2.9-2.9-6.9-4.2-11.1-3.6    C45.2,5.6,42.8,7,40.8,8.9z M41,32c2.8,0,5,2.2,5,5s-2.2,5-5,5s-5-2.2-5-5S38.2,32,41,32z M59,68c-2.8,0-5-2.2-5-5s2.2-5,5-5    s5,2.2,5,5S61.8,68,59,68z M66.8,38.8l-28,28c-1.6,1.6-4.1,1.6-5.7,0c-1.6-1.6-1.6-4.1,0-5.7l28-28c1.6-1.6,4.1-1.6,5.7,0    S68.4,37.3,66.8,38.8z"/></g></switch></svg>
 
         </a>
 
